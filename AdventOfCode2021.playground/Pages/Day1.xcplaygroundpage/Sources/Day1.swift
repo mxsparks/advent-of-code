219045521e44221b785e79c320d15fe15f34fbe5
Clara Sparks <43120704+mxsparks@users.noreply.github.com>
2021-12-01 19:57:50 -0500

    Day1: factor out the sliding sum

--- AdventOfCode2021.playground/Pages/Day1.xcplaygroundpage/Sources/Day1.swift
+++ AdventOfCode2021.playground/Pages/Day1.xcplaygroundpage/Sources/Day1.swift
@@ -15,18 +15,13 @@ public func day1_part1(_ inputs: [Int]) -> Int {
 public func day1(_ inputs: [Int], windowSize: Int = 1) -> Int {
     var retval = 0
     var window: [Int] = []
-    var sum = 0
     for value in inputs {
-        if window.count == windowSize {
+        if window.count == windowSize
+            && value > window.removeFirst()
+        {
             // Array is a very bad queue for large window sizes
             // since removeFirst() is O(n), but it'll do for now
-            let newSum = sum - window.removeFirst() + value
-            if newSum > sum {
-                retval += 1
-            }
-            sum = newSum
-        } else {
-            sum += value
+            retval += 1
         }
         window.append(value)
     }
